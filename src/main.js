f71a7d4ba42dc20c3e23448cf8384c238d6bf8f1
bodyscrap <svkcyberbots@gmail.com>
2024-09-02 22:50:13 +0900

    注目点セレクタの実装

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ let _imageCanvas = document.querySelector("#imageCanvas");
 let _maskCanvas = document.querySelector("#maskCanvas");
 let _selectCanvas = document.querySelector("#selectCanvas");
 let _bufImage = _imageCanvas.getContext("2d");
+let selectedPos = null;
+let isImageLodaed = false;
 
 // DOMの読み込み完了後に行う処理
 window.addEventListener("DOMContentLoaded", () => {
   // イベントの登録
   document.querySelector("#btnOpen").addEventListener("click", openImage);
+  _selectCanvas.addEventListener("click", selectPos);
 });
 
 function openImage() 
@@ -33,5 +36,43 @@ function openImage()
       img.data[i] = data[2][i]
     }
     _bufImage.putImageData(img,0,0);
+    // 選択解除
+    clearMarker(selectedPos);
+    selectPos = null;
+    isImageLodaed = true;
   });
+}
+
+function selectPos(e)
+{
+  if(isImageLodaed == true) {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    let pos = { x: x, y: y };
+    console.log(pos);
+    clearMarker(selectedPos);
+    drawMarker(pos, selectedPos);
+    selectedPos = pos
+  }
+}
+
+function drawMarker(pos)
+{
+  let ctx = _selectCanvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(pos.x, pos.y, 5, 0, Math.PI * 2, false);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function clearMarker(pos)
+{
+  // 前回の円をカバーする範囲を透明な矩形でクリア
+  if (pos != null) {
+    let ctx = _selectCanvas.getContext("2d");
+    ctx.clearRect(pos.x - 7, pos.y - 7, 14, 14);
+  }
 }
